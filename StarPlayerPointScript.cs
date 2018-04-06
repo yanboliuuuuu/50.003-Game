191ee9023a321d02467a9813671ce5e2f4e72ad4
yanboliuuuuu <yanboliuuuuu@users.noreply.github.com>
2018-04-06 22:28:56 +0800

    Add files via upload

---
+++ StarPlayerPointScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarPlayerPointScript : MonoBehaviour {
+	SpriteBehaviour player;
+	//private int point = 1;
+	//public bool mapHaveStar = false;
+	float rotSpeed = 60;
+
+	public Vector3 position;
+	void Start () {
+		player = FindObjectOfType<SpriteBehaviour> ();	
+	}
+
+	void Update(){
+		transform.Rotate(0, rotSpeed * Time.deltaTime, 0, Space.World);
+	}
+
+	void OnTriggerEnter2D(Collider2D hit){
+		if (hit.CompareTag ("Player")) {
+			Debug.Log ("collide with star");
+			player.points += 2;
+			//mapHaveStar = false;
+			Destroy (gameObject);
+		}
+	}
+}
+
